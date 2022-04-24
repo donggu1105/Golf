2e0d318da5ac98dabf0a5410bc8c64a477467900
donggu1105 <kbh11051@gmail.com>
2022-04-24 17:23:33 +0900

    getter

--- src/main/java/com/tg/golf/exception/ErrorResponse.java
+++ src/main/java/com/tg/golf/exception/ErrorResponse.java
@@ -2,6 +2,7 @@ package com.tg.golf.exception;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 
@@ -9,6 +10,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+
+@Getter
 public class ErrorResponse {
 
     private LocalDateTime timestamp = LocalDateTime.now();
